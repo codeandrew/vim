ffeb4481fcbd744d7ec475c6953025d43cded742
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-08-04 10:56:52 +0800

    Add argument in install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Finish Cloning vundle"
 
-cp .vimrc ~
+cp -rfvi .vimrc ~
 echo "Finish copying vimrc" 
 
 vim +PluginInstall +qall
 echo "Finish Plugin Install in Vim" 
 
-cp -rfv plugin syntax ~/.vim
+cp -rfvi plugin syntax ~/.vim
 echo "Finish copying syntax and plugin configs"
