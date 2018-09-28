e05a51a7e90150e01c086cbd61d4b38425b3e652
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-09-28 13:46:30 +0800

    Organize instruction

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "Finish Cloning vundle"
 cp -rfvi .vimrc ~
 echo "Finish copying vimrc" 
 
-vim +PluginInstall +qall
-echo "Finish Plugin Install in Vim" 
-
 cp -rfvi .vim/plugin .vim/syntax ~/.vim
 echo "Finish copying syntax and plugin configs"
+
+vim +PluginInstall +qall
+echo "Finish Plugin Install in Vim" 
